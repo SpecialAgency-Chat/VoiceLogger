0e47eb2583c4869c21c8368b693363ebc61dbc01
waki285 <suzuneu@suzuneu.com>
2023-07-31 10:33:29 +0900

    Update ESLint configuration to warn on unused variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es2021": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  },
+  "rules": {
+    "no-unused-vars": "warn",
+  }
 }
